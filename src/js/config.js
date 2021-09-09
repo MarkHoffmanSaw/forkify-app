8e92c7ffba73f06f092e824ab8492e8f7c97481b
Artur <tullers@mail.ru>
2021-09-09 13:47:25 +0300

    added "num pages"

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd12c0cbc-0a78-42cf-bbe2-ad9bba2070f3';
+export const MODAL_CLOSE_SEC = 2.5;
