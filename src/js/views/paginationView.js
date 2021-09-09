94520e4b2cb7fc58595ab0c4383ded686a16a119
Artur <tullers@mail.ru>
2021-09-09 16:45:01 +0300

    curr.page

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -20,7 +20,7 @@ class PaginationView extends View {
       this._data.results.length / this._data.resultsPerPage
     );
     const allPages = `
-      <span class="pagination__pages">Total pages: ${this._data.results.length}</span>
+      <span class="pagination__pages">Page: ${curPage}</span>
     `;
 
     // Page 1, and there are other pages
